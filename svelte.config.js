b6d1365b471dbc20062102a673f0511249eb81ce
BenCinn <pp.rongpan@gmail.com>
2022-05-11 18:14:30 +0700

    Change adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
+import static_adapter from '@sveltejs/adapter-static';
 import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.md'],
-	preprocess: [
-		mdsvex({
-			extensions: ['.md'],
-			layout: {
-				blog: 'src/routes/blog/_post.svelte'
-			}
-		})
-	],
-	kit: {
-		adapter: adapter(),
+  extensions: ['.svelte', '.md'],
+  preprocess: [
+    mdsvex({
+      extensions: ['.md'],
+      layout: {
+        blog: 'src/routes/blog/_post.svelte'
+      }
+    })
+  ],
+  kit: {
+    adapter: static_adapter(),
 
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
-	}
+    // Override http methods in the Todo forms
+    methodOverride: {
+      allowed: ['PATCH', 'DELETE']
+    }
+  }
 };
 
 export default config;
