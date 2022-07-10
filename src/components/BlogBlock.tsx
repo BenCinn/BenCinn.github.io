91fededf8ca167dd79e25bc42735e474063cf99e
Chitsanupong Rongpan <pp.rongpan@gmail.com>
2022-07-10 13:34:05 +0700

    Change some stuff

--- src/components/BlogBlock.tsx
+++ src/components/BlogBlock.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 
 export default function Blog(props) {
-  if (props.latest == false){
       return (
         <Link href={`blog/${props.slug}`}>
           <a><div className="overflow-hidden max-w-sm rounded shadow-lg transition duration-300 ease-in-out hover:shadow-xl">
@@ -21,36 +20,11 @@ export default function Blog(props) {
                 <span key={tag} className="inline-block px-3 py-1 mr-2 mb-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">{tag}</span>
               )
           })}
+          {props.latest == false ? '' : <span className="inline-block px-3 py-1 mr-2 mb-2 text-sm font-semibold text-gray-700 bg-yellow-200 rounded-full">newest</span>}
         </div>
       </div></a>
           
         </Link>
         
     );
-  } else {
-    return (
-      <Link href={`blog/${props.slug}`}>
-      <a><div className="overflow-hidden max-w-sm rounded shadow-lg transition duration-300 hover:shadow-xl">
-      <img className="w-full" src={props.img} alt={props.text} />
-      <div className="px-6 py-4">
-      <div className="mb-2 text-xl font-bold">{props.text}</div>
-      <p className="text-base text-gray-700">
-        {props.desc}
-      </p>
-      </div>
-      <div className="px-6 pt-4 text-base text-gray-700">
-        <span>Posted: {props.date}</span>
-      </div>
-      <div className="px-6 pt-4 pb-3">
-        {props.tags.map(tag => {
-            return (
-              <span className="inline-block px-3 py-1 mr-2 mb-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">{tag}</span>
-            )
-        })}
-        <span className="inline-block px-3 py-1 mr-2 mb-2 text-sm font-semibold text-gray-700 bg-yellow-200 rounded-full">newest</span>
-      </div>
-    </div></a></Link>
-  );
-  }
-  
 }
